458554c74859f0daa35fdd38443ea67ea6c8d31e
AllaOz <ozcanalla@gmail.com>
2023-03-05 15:47:28 +0300

    test

--- index.js
+++ index.js
@@ -1,17 +1,27 @@
-const form = document.querySelector('.form__comment');
-const commentsDiv = document.getElementById('show__comments');
-
-button = document.getElementById("submit");
-button.addEventListener("click", function(event){
-  event.preventDefault();
-  const name = document.getElementById('name').value;
-  const link = document.getElementById('link').value;
-  const comment = document.getElementById('comment-field').value;
-
- const commentElem = document.createElement('div');
-  commentElem.innerHTML = `
-    <p>${name}</p>
-    <p>${link}</p>
-    <p>${comment}</p>
-  `;
-commentsDiv.appendChild(commentElem);});
+function changeName(name) {
+    return name.trim().replace(/\b\w/g, (l) => l.toUpperCase());
+  }
+  function checkSpam(comment) {
+    return comment.replace(/viagra|XXX/gi, '***');
+  }
+  
+  const form = document.querySelector('.form__comment');
+  const commentsDiv = document.getElementById('show__comments');
+  const button = document.getElementById('submit');
+  
+  button.addEventListener('click', function (event) {
+    event.preventDefault();
+  
+    const name = changeName(document.getElementById('name').value);
+    const link = document.getElementById('link').value;
+    const comment = checkSpam(document.getElementById('comment-field').value);
+   
+    const commentElem = document.createElement('div');
+    commentElem.innerHTML = `
+      <p>${name}</p>
+      <p><img src="${link}"></p>
+      <p>${comment}</p>
+    `;
+    commentsDiv.appendChild(commentElem);
+  });
+  
